fe46a52865c6e66773176919f12a3d045a5d555f
Aggio98 <nigelrizzo@gmail.com>
2022-09-22 13:25:49 +0200

    Message to students

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log(
+  "This course is not easy, it is no walk in the park. It takes someone to got to the end and finish it and get a job. You are one of these people! Having already signed up for the course you are half way to changing your life forever. Keep going and never stop! You have what it takes I promise."
+);
